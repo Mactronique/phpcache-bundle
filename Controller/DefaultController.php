a1f01241f9f8c42704099636b52f58ff872c0e7d
macintoshplus <jean-baptiste@nahan.fr>
2015-07-23 18:34:46 +0200

    example of use

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,26 @@
 namespace Mactronique\Bundle\PhpCacheBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Mactronique\PhpCache\Driver\XcacheDriver;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('MactroniquePhpCacheBundle:Default:index.html.twig', array('name' => $name));
+        $cache = $this->get("mactronique_cache.phpcache");
+        $cache->set('my', $name, null, XcacheDriver::NAME);
+        $val = $cache->get('my', XcacheDriver::NAME);
+        return $this->render('MactroniquePhpCacheBundle:Default:index.html.twig', array('name' => $val));
+    }
+
+    public function showAction()
+    {
+        $cache = $this->get("mactronique_cache.phpcache");
+        if ($cache->exists('my')) {
+            $val = $cache->get('my', XcacheDriver::NAME);
+        } else {
+            $val = "No name !";
+        }
+        return $this->render('MactroniquePhpCacheBundle:Default:index.html.twig', array('name' => $val." (from cache)"));
     }
 }
